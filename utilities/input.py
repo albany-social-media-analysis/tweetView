0e6500fa241f9dfefb612ae995e59b750da585df
sjacks26 <sjacks26@gmail.com>
2020-05-19 12:28:25 -0400

    Streamline input.py Remove functionality to write to mongo Use pandas to read data from all file types

--- utilities/input.py
+++ utilities/input.py
@@ -1,68 +1,58 @@
-from pymongo import MongoClient
+"""
+To build master data collections, tweetView reads in specified data from a file.
+File types supported: txt, csv, json
+See input data template files in templates/data_templates/input/ for examples of how to structure input data
+
+"""
+
+
 from pathlib import Path
-import mongo_config
-from csv import DictReader
 import pandas as pd
 
+import mongo_config
 
-def input_data_to_master_data(db_login, user_name_login, pwd_login, project_name, data_file_path):
-    authorized_client = MongoClient(db_login, username=user_name_login, password=pwd_login, path=mongo_config.path)
+class FileInputError(Exception):
+    def __init__(self, *args):
+        if args:
+            self.message = args[0]
+        else:
+            self.message = None
 
-    # check if project has been initialized
-    system_dbs = authorized_client.list_database_names()
-    system_dbs = [db['name'] for db in system_dbs]
+    def __str__(self):
+        if self.message:
+            return self.message
+        else:
+            return "Unspecified FileInputError"
 
-    if project_name in system_dbs:
 
-        # file assessment
-        data_file_path = Path(data_file_path)
-        if data_file_path.exists():
+def read_data_from_file(data_file_path):
+    data_file_path = Path(data_file_path)
 
-            if data_file_path.is_file():
+    if data_file_path.is_file():
 
-                if data_file_path.suffix == '.csv':
-                    with open(data_file_path, 'r') as csv_file:
-                        dataset_reader = DictReader(csv_file)
-                        master_data = [dict(row) for row in dataset_reader]
-                    md_collection = [authorized_client[project_name]['MASTER'].insert_one(doc) for doc in master_data]
-                    approved_message = data_file_path.name, "has been successfully uploaded as the project's master " \
-                                                            "dataset"
-                    return approved_message
+        if data_file_path.suffix == '.csv':
+            master_data = pd.read_csv(data_file_path)
+            return master_data
 
-                elif data_file_path.suffix == '.json':
-                    try:
-                        json_file = pd.read_json(data_file_path, orient='records')
-                    except ValueError:
-                        json_file = pd.read_json(data_file_path, lines=True)
-                    rows = json_file.iterrows()
-                    master_data = [dict(row.__next__()[1]) for row in rows]
-                    md_collection = [authorized_client[project_name]['MASTER'].insert_one(doc) for doc in master_data]
-                    approved_message = data_file_path.name, "has been successfully uploaded as the project's master" \
-                                                            " dataset"
-                    return approved_message
+        elif data_file_path.suffix == '.json':
+            try:
+                master_data = pd.read_json(data_file_path, orient='records') # This expects either a single json object or a list of json objects. Corresponds to input_data_template1.json
+            except ValueError:
+                try:
+                    master_data = pd.read_json(data_file_path, orient='records', typ='series', convert_dates=False) # This expects either a single json object or a list of json objects with only 1 key:value pair, treating the key as the index for the resulting object. Corresponds to input_data_template2.json
+                except ValueError:
+                    master_data = pd.read_json(data_file_path, lines=True) # This expects one json object per line. Corresponds to input_data_template3.json
+            return master_data
 
-                elif data_file_path.suffix == '.txt':
-                    txt_file = pd.read_fwf(data_file_path)
-                    rows = txt_file.iterrows()
-                    master_data = [dict(row.__next_()[1]) for row in rows]
-                    md_collection = [authorized_client[project_name]['MASTER'].insert_one(doc) for doc in master_data]
-                    approved_message = data_file_path.name, "has been successfully uploaded as the project's master" \
-                                                            " dataset"
-                    return approved_message
-
-                else:
-                    denied_message = "Unsupported file type. Tweet view only supports .csv, .txt, and .json file types"
-                    return denied_message
-
-            else:
-                denied_message = "This is not a valid file path"
-                return denied_message
+        elif data_file_path.suffix == '.txt':
+            master_data = pd.read_csv(data_file_path) # This expects a txt file with a header in the first row. Corresponds to input_data_template1.txt
+            if not 'tweet_ids' in master_data.columns:
+                master_data = pd.read_csv(data_file_path, names=['tweet_ids']) # This expects a txt file with no header row. Corresponds to input_data_template2.txt
+            return master_data
 
         else:
-            denied_message = "The file path specified does not exist"
-            return denied_message
+            raise FileInputError("Unsupported file type. tweetView only supports .csv, .txt, and .json file types")
 
     else:
+        raise FileInputError("The file path specified does not exist")
 
-        denied_message = "The project name you entered does not exist"
-        return denied_message
