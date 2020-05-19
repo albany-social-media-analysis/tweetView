bee26987a79da214d7c13d1b40ab292b0bb8fca9
sjacks26 <sjacks26@gmail.com>
2020-05-19 14:39:27 -0400

    Mongo config file template was missing!

---
+++ mongo_config_template.py
@@ -0,0 +1,6 @@
+port = 27017  # This is the default port for Mongo. Change this if your Mongo instance is using a different port
+user =
+pwd =
+
+tv_admin = # This is a root user created by initialize_tweet_view.py. tweetView uses this user for all background tasks.
+tv_admin_pwd =
