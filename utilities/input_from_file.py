a895e128feeae69cb31c20b1a5323eec5b1c94e2
sjacks26 <sjacks26@gmail.com>
2020-05-19 14:12:42 -0400

    Rename input file, make output of that file a list of tweet ids

--- utilities/input_from_file.py
+++ utilities/input_from_file.py
@@ -3,6 +3,7 @@ To build master data collections, tweetView reads in specified data from a file.
 File types supported: txt, csv, json
 See input data template files in templates/data_templates/input/ for examples of how to structure input data
 
+master_data is a list of tweet ids with "ID_" prepended to each
 """
 
 
@@ -30,6 +31,12 @@ def read_data_from_file(data_file_path):
 
         if data_file_path.suffix == '.csv':
             master_data = pd.read_csv(data_file_path)
+            if type(master_data) == pd.core.frame.DataFrame:
+                column_name = master_data.columns[0]
+                master_data = master_data[column_name].to_list()
+            elif type(master_data) == pd.core.series.Series:
+                master_data = master_data.index.to_list()
+
             return master_data
 
         elif data_file_path.suffix == '.json':
@@ -40,12 +47,26 @@ def read_data_from_file(data_file_path):
                     master_data = pd.read_json(data_file_path, orient='records', typ='series', convert_dates=False) # This expects either a single json object or a list of json objects with only 1 key:value pair, treating the key as the index for the resulting object. Corresponds to input_data_template2.json
                 except ValueError:
                     master_data = pd.read_json(data_file_path, lines=True) # This expects one json object per line. Corresponds to input_data_template3.json
+
+            if type(master_data) == pd.core.frame.DataFrame:
+                column_name = master_data.columns[0]
+                master_data = master_data[column_name].to_list()
+            elif type(master_data) == pd.core.series.Series:
+                master_data = master_data.index.to_list()
+
             return master_data
 
         elif data_file_path.suffix == '.txt':
             master_data = pd.read_csv(data_file_path) # This expects a txt file with a header in the first row. Corresponds to input_data_template1.txt
             if not 'tweet_ids' in master_data.columns:
                 master_data = pd.read_csv(data_file_path, names=['tweet_ids']) # This expects a txt file with no header row. Corresponds to input_data_template2.txt
+
+            if type(master_data) == pd.core.frame.DataFrame:
+                column_name = master_data.columns[0]
+                master_data = master_data[column_name].to_list()
+            elif type(master_data) == pd.core.series.Series:
+                master_data = master_data.index.to_list()
+
             return master_data
 
         else:
